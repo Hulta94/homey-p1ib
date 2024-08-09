aad7964395a9c9058fcc3fc942c03ef2e2d68ea3
Albin Hultgren <hultgren.albin@gmail.com>
2024-08-10 01:15:29 +0200

    Added Power Export Total trigger card

--- drivers/p1ib/driver.js
+++ drivers/p1ib/driver.js
@@ -24,6 +24,7 @@ class P1ibDriver extends Driver {
     });
 
     this.homey.flow.getDeviceTriggerCard('meter_power.export_changed');
+    this.homey.flow.getDeviceTriggerCard('measure_power.export_changed');
   }
 
   handleDiscoveryResult(discoveryResult) {
